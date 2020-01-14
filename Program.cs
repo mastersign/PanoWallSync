8ebbf5cc8ee6497befdd8661b2823108e992ec95
Tobias Kiertscher <dev@mastersign.de>
2020-01-14 07:26:02 +0100

    imported project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Mastersign.PanoWall.Client
+{
+    class Program
+    {
+        const string baseUrl = "https://panowall.net/";
+
+        static void Main(string[] args)
+        {
+            var pathRegex = new Regex(@"^(?:\w\d+)+_(?:\d+p)$");
+            var cwd = Directory.GetCurrentDirectory();
+            var targetFolders = Directory.EnumerateDirectories(cwd)
+                .Where(p => pathRegex.IsMatch(Path.GetFileName(p)))
+                .ToArray();
+            foreach (var targetFolder in targetFolders)
+            {
+                var setup = Path.GetFileName(targetFolder);
+                Console.WriteLine(targetFolder);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -16,7 +16,7 @@ for a multi-screen-setup with high resolution up to 8K.
 
 * Copy `PanoWallSync.exe` from the _Releases_ section to a folder of your choice
 * Create a folder for every resolution you whould like to download  
-  E.g. for a two-screen-setup with vertical resolution of 1440px create a folder `1440pH2`
+  E.g. for a horizontal two-screen-setup with vertical resolution of 1440px create a folder `h2_1440p`
 
 ## Download PanoWall pictures
 
